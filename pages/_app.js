47fc328314e13be57447bda47df85bd0d203baa5
Drissxnew <143014858+Drissxnew@users.noreply.github.com>
2023-08-25 16:37:17 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 // pages/_app.js
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+import { AppProvider } from '../context/AppContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AppProvider>
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    </AppProvider>
+  );
 }
 
 export default MyApp;
