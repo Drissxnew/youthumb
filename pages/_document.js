f7f594675e44af4f75732c4d4a7d5cc0e0529339
Drissxnew <143014858+Drissxnew@users.noreply.github.com>
2023-08-25 16:34:00 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,12 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html>
         <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
+          {/* Add meta tags, stylesheets, etc. */}
         </Head>
         <body>
           <Main />
