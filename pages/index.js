ada11a76acd5c53e9aa8bfc80bbe6cd93b07e7be
Drissxnew <143014858+Drissxnew@users.noreply.github.com>
2023-08-25 16:32:06 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,83 +1,29 @@
-import { useState } from "react";
-import copy from "copy-to-clipboard";
+const express = require('express');
+const ytdl = require('ytdl-core');
+const app = express();
+const port = 3000;
 
-const Index = () => {
-  const [videoURL, setVideoURL] = useState("");
-  const [thumbnailOptions, setThumbnailOptions] = useState([]);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
 
-  const getYouTubeThumbnail = (url) => {
-    let regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
-    let match = url.match(regExp);
+app.get('/download', (req, res) => {
+    const videoURL = req.query.url;
 
-    if (match && match[1].length === 11) {
-      const videoURL = match[1];
-      const thumbnailBaseUrl = "http://img.youtube.com/vi/";
-
-      const options = [
-        { resolution: "HD (1280x720)", code: "maxresdefault" },
-        { resolution: "SD (640x480)", code: "sddefault" },
-        { resolution: "Normal (480x360)", code: "hqdefault" },
-        { resolution: "Medium (320x180)", code: "mqdefault" },
-        { resolution: "Low (120x90)", code: "default" },
-      ];
-
-      const thumbnailOptions = options.map((option) => ({
-        resolution: option.resolution,
-        url: `${thumbnailBaseUrl}${videoURL}/${option.code}.jpg`,
-      }));
-
-      setThumbnailOptions(thumbnailOptions);
-      setVideoURL("");
-    } else {
-      setThumbnailOptions([]);
+    if (!ytdl.validateURL(videoURL)) {
+        return res.send('Invalid YouTube URL');
     }
-  };
 
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <header className="text-center mb-8">
-        <h1 className="text-3xl font-bold mb-2">
-          Youtube Thumbnail Downloader
-        </h1>
-        <p className="text-gray-600">
-          Download high-quality thumbnails from YouTube videos.
-        </p>
-      </header>
-      <div className="text-center">
-        <input
-          type="text"
-          className="w-full md:w-1/2 px-4 py-2 border rounded"
-          placeholder="Enter YouTube URL"
-          value={videoURL}
-          onChange={(e) => setVideoURL(e.target.value)}
-        />
-        <button
-          className="btn-blue mt-2"
-          onClick={() => getYouTubeThumbnail(videoURL)}
-        >
-          Download Thumbnails
-        </button>
-      </div>
-      {thumbnailOptions.length > 0 && (
-        <div className="mt-8">
-          <h2 className="text-xl font-semibold mb-4">Thumbnail Options</h2>
-          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {thumbnailOptions.map((option, index) => (
-              <div key={index} className="thumbnail-option">
-                <img src={option.url} alt={`Thumbnail ${index + 1}`} />
-                <button
-                  className="btn-blue mt-2"
-                  onClick={() => copy(option.url)}
-                >
-                  Copy Image URL
-                </button>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
-    </div>
-  );
-};
+    const videoInfo = ytdl.getInfo(videoURL);
+    const title = videoInfo.videoDetails.title;
 
-export default Index;
+    res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+
+    ytdl(videoURL, { quality: 'highestaudio' })
+        .pipe(res);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
